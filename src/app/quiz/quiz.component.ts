85a0b32b66daa1d533537d9b7d848c49d08c8409
Bilal Hussain <bilal.hussain@incentro.com>
2018-05-06 12:47:23 +0200

    Implemented ng-containers for Quiz Component, added user data stuff for local storage, more methods but logical and functional, such as checkAnswer, saveAnswer, added shuffle method for array (answers) shuffling

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {QuizService} from './../services/quiz.service';
-import {Observable} from 'rxjs/Observable';
 import { timer } from 'rxjs/observable/timer';
 import { take, map } from 'rxjs/operators';
 
@@ -12,13 +11,23 @@ import { take, map } from 'rxjs/operators';
 })
 
 export class QuizComponent implements OnInit {
-  questions: any;
-  questionsAmount = this.questionsLength();
+  questions: any[];
+  questionsAmount = this.quizService.getMockData().results.length;
   nameQuizzer: string = sessionStorage.getItem('name') || null;
-  answers: string[];
-  answered: {};
-  answerNumber = 1;
+  answerChecked: boolean;
+  userAnswerValue: string;
+  answers: {};
   questionsLeft = this.questionsAmount;
+
+  // User data stuff
+  userData = {};
+  answeredAmount = 0;
+  userAnswers = [];
+  correctAnswers = [];
+  correctAnswer = '';
+  correctAnswersAmount = 0;
+  incorrectAnswersAmount = 0;
+  invalidAnswersAmount = 0;
 
   // Time stuff
   countDown;
@@ -35,106 +44,123 @@ export class QuizComponent implements OnInit {
     );
   }
 
-  nextQuestion(event, answer) {
+  nextQuestion(event, answer, correctAnswer) {
     event.preventDefault();
-    console.log('>>> volgende vraag >>>');
-    console.log('answer: ', answer);
-    this.checkAnswer(answer);
 
+    // Reset time
+    this.countDownTimer(true);
+
+    // Check if answer is (in)correct
+    this.checkAnswer(answer, correctAnswer);
+
+    // Put the answer checkbox (checked) back to false
+    this.answerChecked = false;
+
+    // Go to the next question
     const parentEl = event.target.parentElement;
     const nextSibling = parentEl.nextSibling;
 
     parentEl.classList.add('hidden');
 
-    if (nextSibling  !== 'undefined') {
+    if (nextSibling !== 'undefined') {
       nextSibling.classList.remove('hidden');
     }
 
-    this.countDownTimer(true);
+    console.log('this.userData', this.userData);
   }
 
-  changedCheckbox(event) {
-    const eventTarget = event.target;
+  changedCheckbox(event, answer, answerChecked, correctAnswer) {
+    this.userAnswerValue = answer;
+    this.answerChecked = answerChecked;
+    this.correctAnswer = correctAnswer;
+  }
 
-    if (eventTarget.checked) {
-      console.log('is checked?: ', eventTarget.checked);
-      this.answered = {
-        answered: this.answerNumber++,
-        correct: '',
-        incorrect: ''
+  checkAnswer(answer, correctAnswer) {
+    // If checkbox is checked
+    if (this.answerChecked) {
+      answer === correctAnswer ? ++this.correctAnswersAmount : ++this.incorrectAnswersAmount;
+
+      this.userData = {
+        answered: ++this.answeredAmount,
+        answers: this.userAnswers,
+        answers_correct: this.correctAnswersAmount,
+        answers_incorrect: this.incorrectAnswersAmount,
+        answers_invalid: this.invalidAnswersAmount
       };
 
-      this.questionsLeft--;
-
-      console.log('this.answered: ', this.answered);
-
-    } else {
-      this.answered = {
-        answered: this.answerNumber--,
-        correct: '',
-        incorrect: ''
-      };
-
-      this.questionsLeft++;
-
-      console.log('is checked?: ', eventTarget.checked);
+      this.userAnswers.push(answer);
     }
+
+    // If checkbox isn't checked, the answer is marked as invalid
+    else {
+      ++this.invalidAnswersAmount;
+
+      this.userData = {
+        answered: this.answeredAmount,
+        answers: this.userAnswers,
+        answers_correct: this.correctAnswersAmount,
+        answers_incorrect: this.incorrectAnswersAmount,
+        answers_invalid: this.invalidAnswersAmount
+      };
+    }
+
+    // One more question lesser, because the question is now answered
+    this.questionsLeft--;
+
+    // Save answer data
+    this.saveAnswer();
   }
 
-  checkAnswer(answer) {
-    // if answer is correct, then edit the object with answers_correct: +1
-    // else anwers_incorrect: +1
-    // checking the answer should be done with comparing it to the original JSON (api)
-    console.log('>>> check of antwoord correct is: ', answer);
-  }
-
-  questionsLength() {
-    return this.quizService.getMockData().results.length;
+  saveAnswer() {
+    localStorage.setItem('userData', JSON.stringify(this.userData));
   }
 
   getQuestions() {
-      this.quizService.getQuestionsAPI().subscribe(questions => this.questions = questions.results);
+    this.quizService.getQuestionsAPI().subscribe(questions => {
+      this.questions = questions.results;
+
+      let allAnswers: string[];
+      const allQuestions = this.questions;
+      for (const i of allQuestions) {
+        this.correctAnswers = [i.correct_answer];
+        const incorrectAnswers = i.incorrect_answers;
+        allAnswers = this.shuffle([...this.correctAnswers, ...incorrectAnswers]);
+        this.answers = {'allAnswers': allAnswers};
+        i.allAnswers = allAnswers;
+      }
+    });
   }
 
-
-
-  // TODO's
-  showResults() {
-    console.log('show results');
-  }
-
-  showEaster(event) {
+  showResults(event, answer, correctAnswer) {
     event.preventDefault();
-    const easterContent = document.querySelector('.noShow');
-    easterContent.classList.remove('noShow');
+
+    this.checkAnswer(answer, correctAnswer);
+    console.log('this.userData', this.userData);
   }
 
-  resetShit() {
-    console.log('alles moet hiermee gereset worden');
+  // TODO: Make a helper function/polyfill for this
+  shuffle(array) {
+    let counter = array.length;
+
+    // While there are elements in the array
+    while (counter > 0) {
+      // Pick a random index
+      const index = Math.floor(Math.random() * counter);
+
+      // Decrease counter by 1
+      counter--;
+
+      // And swap the last element with it
+      const temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+    }
+
+    return array;
   }
-
-  // getAllAnswers() {
-  //   this.quizService.getMockData().results.map(i => {
-  //     // console.log('i', i);
-  //
-  //     let allAnswers: any[];
-  //
-  //     // console.log('i ', i.incorrect_answers);
-  //
-  //     const correctAnswer = [i.correct_answer];
-  //     const incorrectAnswers = i.incorrect_answers;
-  //
-  //     allAnswers = [...correctAnswer, ...incorrectAnswers];
-  //     this.answers = allAnswers;
-  //   });
-  // }
-
-
 
   ngOnInit() {
     this.countDownTimer(false);
     this.getQuestions();
-
-    // this.getAllAnswers();
   }
 }
