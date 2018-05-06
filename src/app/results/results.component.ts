f1f520150dd6b6a9ee5f5849d3f29018154fd341
Bilal Hussain <bilal.hussain@incentro.com>
2018-05-06 12:48:03 +0200

    Refactored results, based on data of localStorage

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,22 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
   styleUrls: ['./results.component.css']
 })
+
 export class ResultsComponent implements OnInit {
+  userData = localStorage.getItem('userData');
+  parsedData = JSON.parse(this.userData);
+  questionAnsweredAmount = this.parsedData.answered;
+  correctAnswers = this.parsedData.answers_correct;
+  incorrectAnswers = this.parsedData.answers_incorrect;
+  invalidAnswers = this.parsedData.answers_invalid;
+  perfectScore: boolean;
+  badScore: boolean;
 
   constructor() { }
 
   ngOnInit() {
-    this.newQuiz();
+    this.calculateScore();
   }
 
+  calculateScore() {
+    if (this.correctAnswers === 3) {
+      this.perfectScore = true;
+    } else if (this.correctAnswers < 2) {
+      this.badScore = true;
+    }
+  }
 
+  resetData() {
+    localStorage.removeItem('userData');
+  }
 
-  newQuiz() {
-    // TODO: Remove all data except name
-    console.log('new quiz!');
+  newQuiz(event) {
+    event.preventDefault();
+    this.resetData();
   }
 }
