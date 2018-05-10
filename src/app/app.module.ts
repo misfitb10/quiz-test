d2b3bf600fb408716851d330cbdc2fe35b61146c
Bilal Hussain <bilal.hussain@incentro.com>
2018-05-10 15:43:17 +0200

    Added HashLocationStrategy because of reload error, and added page not found component for 404 errors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
+import {Routing} from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
 import { NameComponent } from './name/name.component';
 import { ResultsComponent } from './results/results.component';
-import {Routing} from './app.routes';
-import { HttpClientModule } from '@angular/common/http';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NameComponent,
     QuizComponent,
-    ResultsComponent
+    ResultsComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule, Routing, HttpClientModule
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { RouterModule, Routes } from '@angular/router';
 import {NameComponent} from './name/name.component';
 import {QuizComponent} from './quiz/quiz.component';
 import {ResultsComponent} from './results/results.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: NameComponent },
   { path: 'intro', component: NameComponent },
   { path: 'quiz', component: QuizComponent },
-  { path: 'results', component: ResultsComponent }
+  { path: 'results', component: ResultsComponent },
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 export const Routing = RouterModule.forRoot(routes);
